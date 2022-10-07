0b0660b57c44afdaba3ec40ce31b68009e996fa2
giovannijunseokim <kingjunseo@gmail.com>
2022-10-07 13:20:40 +0900

    [feat] id, pw 받아올 때 널 안정성 추가 [UI] 메인 화면(홈 화면) xml 파일 작성 완료

--- app/src/main/java/org/sopt/sample/LoginActivity.kt
+++ app/src/main/java/org/sopt/sample/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
@@ -13,8 +14,8 @@ class LoginActivity : AppCompatActivity() {
 
     lateinit var resultLauncher: ActivityResultLauncher<Intent>
     lateinit var binding: ActivityLoginBinding
-    lateinit var id: String
-    lateinit var pw : String
+    var id: String? = null
+    var pw : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,47 +23,44 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    id = result.data?.getStringExtra("id") ?: ""
+                    pw = result.data?.getStringExtra("pw") ?: ""
+                }
+            }
+
         binding.loginBtn.setOnClickListener {
-            if (binding.idEt.text.length >= 6) {
+
+            if (binding.idEt.text.toString() == id || binding.pwET.text.toString() == pw) {
                 loginSuccess()
             } else {
                 loginFail()
             }
         }
         binding.signupBtn.setOnClickListener {
-
+            clickSignUp()
         }
     }
 
     fun loginSuccess() {
-        if(binding.idEt.text.toString() == "123" || binding.pwET.text.toString() == pw) {
-            Snackbar.make(binding.root, "로그인 성공", Snackbar.LENGTH_SHORT).show()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        Snackbar.make(
+            binding.root, "로그인 성공", Snackbar.LENGTH_SHORT).show()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
     }
 
     fun loginFail() {
         Snackbar.make(
-            binding.root, "로그인 실패", Snackbar.LENGTH_SHORT
-        ).apply {
-            anchorView = binding.pwTV // 특정뷰 위로 스낵바 이동
-        }.show()
+            binding.root, "로그인 실패", Snackbar.LENGTH_SHORT).show()
     }
 
     fun clickSignUp() {
-        binding.signupBtn.setOnClickListener {
-
-            resultLauncher =
-                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                    if (result.resultCode == Activity.RESULT_OK) {
-                        id = result.data?.getStringExtra("id") ?: ""
-                        pw = result.data?.getStringExtra("pw") ?: ""
-                    }
-                }
-            val intent = Intent(this, SignUpActivity::class.java)
-            resultLauncher.launch(intent)
-        }
+        val intent = Intent(this, SignUpActivity::class.java)
+        resultLauncher.launch(intent)
     }
 }
