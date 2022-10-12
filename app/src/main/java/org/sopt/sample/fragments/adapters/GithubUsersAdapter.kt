d278d38829729c472dadea665ef59f720d89583d
giovannijunseokim <kingjunseo@gmail.com>
2022-10-12 18:12:55 +0900

    [feat] 리스트 수정 및 뷰 모델의 타입에 따라 알맞는 뷰 홀더 제공하도록 코드 수정, 리사이클러 뷰와 텍스트 같이 올라가도록 수정 완료

--- app/src/main/java/org/sopt/sample/fragments/adapters/GithubUsersAdapter.kt
+++ app/src/main/java/org/sopt/sample/fragments/adapters/GithubUsersAdapter.kt
@@ -1,23 +1,47 @@
 package org.sopt.sample.fragments.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.NonDisposableHandle.parent
+import org.sopt.sample.databinding.ItemDescriptionBinding
 import org.sopt.sample.fragments.models.UserRepo
 import org.sopt.sample.databinding.ItemUsersBinding
 
-class GithubUsersAdapter(context: Context):RecyclerView.Adapter<GithubUsersAdapter.GithubUsersViewHolder>(){
+class GithubUsersAdapter(context: Context):RecyclerView.Adapter<RecyclerView.ViewHolder>(){
     private val inflater by lazy { LayoutInflater.from(context) }
     private var userList: List<UserRepo> = emptyList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GithubUsersViewHolder {
-        val binding = ItemUsersBinding.inflate(inflater, parent, false)
-        return GithubUsersViewHolder(binding)
+    override fun getItemViewType(position: Int): Int {
+        return userList[position].dataType
     }
 
-    override fun onBindViewHolder(holder: GithubUsersViewHolder, position: Int) {
-        holder.onBind(userList[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            type1 -> {
+                val binding = ItemDescriptionBinding.inflate(inflater, parent, false)
+                DescriptionViewHolder(binding)
+            }
+            else -> {
+                val binding = ItemUsersBinding.inflate(inflater, parent, false)
+                GithubUsersViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(userList[position].dataType){
+            type1 -> {
+                holder as DescriptionViewHolder
+                holder.onBind(userList[position])
+            }
+            else -> {
+                holder as GithubUsersViewHolder
+                holder.onBind(userList[position])
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -31,9 +55,18 @@ class GithubUsersAdapter(context: Context):RecyclerView.Adapter<GithubUsersAdapt
 
     inner class GithubUsersViewHolder(private val binding: ItemUsersBinding) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data : UserRepo){
-            binding.ivGithubLogo.setImageResource(data.image)
+            binding.ivGithubLogo.setImageResource(data.image!!)
             binding.tvRepoName.text = data.repositoryName
             binding.tvUserName.text = data.name
         }
+    }
+    inner class DescriptionViewHolder(private val binding: ItemDescriptionBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data : UserRepo){
+            binding.tvRepoDescription.text = data.name
+        }
+    }
+    companion object{
+        const val type1 = 1
+        const val type2 = 2
     }
 }
--- app/src/main/java/org/sopt/sample/fragments/models/HomeViewModel.kt
+++ app/src/main/java/org/sopt/sample/fragments/models/HomeViewModel.kt
@@ -2,58 +2,74 @@ package org.sopt.sample.fragments.models
 
 import androidx.lifecycle.ViewModel
 import org.sopt.sample.R
+import org.sopt.sample.fragments.adapters.GithubUsersAdapter.Companion
+
 
 class HomeViewModel : ViewModel(){
     val exampleList = listOf<UserRepo>(
         UserRepo(
-            image = R.drawable.github,
-            name = "Kim Junseo",
-            repositoryName = "Youth-Pick "
+            image = null,
+            name = "junseo-repo",
+            repositoryName = null,
+            dataType = Companion.type1
         ),
         UserRepo(
             image = R.drawable.github,
             name = "Kim Junseo",
-            repositoryName = "junstagram"
-        ),
-        UserRepo(
-            image = R.drawable.github,
-            name = "Kim Junseo",
-            repositoryName = "AndroidLab"
-        ),
-        UserRepo(
-            image = R.drawable.github,
-            name = "Kim Junseo",
-            repositoryName = "print"
-        ),
-        UserRepo(
-            image = R.drawable.github,
-            name = "Kim Junseo",
-            repositoryName = "IN-SOPT-ANDROID / junseo-kim"
+            repositoryName = "Youth-Pick",
+            dataType = Companion.type2
         ),UserRepo(
             image = R.drawable.github,
             name = "Kim Junseo",
-            repositoryName = "Youth-Pick "
+            repositoryName = "junstagram",
+            dataType = Companion.type2
         ),
         UserRepo(
             image = R.drawable.github,
             name = "Kim Junseo",
-            repositoryName = "junstagram"
+            repositoryName = "AndroidLab",
+            dataType = Companion.type2
         ),
         UserRepo(
             image = R.drawable.github,
             name = "Kim Junseo",
-            repositoryName = "AndroidLab"
+            repositoryName = "print",
+            dataType = Companion.type2
         ),
         UserRepo(
             image = R.drawable.github,
             name = "Kim Junseo",
-            repositoryName = "print"
+            repositoryName = "IN-SOPT-ANDROID / junseo-kim",
+            dataType = Companion.type2
+        ),UserRepo(
+            image = R.drawable.github,
+            name = "Kim Junseo",
+            repositoryName = "Youth-Pick",
+            dataType = Companion.type2
         ),
         UserRepo(
             image = R.drawable.github,
             name = "Kim Junseo",
-            repositoryName = "IN-SOPT-ANDROID / junseo-kim"
+            repositoryName = "junstagram",
+            dataType = Companion.type2
+        ),
+        UserRepo(
+            image = R.drawable.github,
+            name = "Kim Junseo",
+            repositoryName = "AndroidLab",
+            dataType = Companion.type2
+        ),
+        UserRepo(
+            image = R.drawable.github,
+            name = "Kim Junseo",
+            repositoryName = "print",
+            dataType = Companion.type2
+        ),
+        UserRepo(
+            image = R.drawable.github,
+            name = "Kim Junseo",
+            repositoryName = "IN-SOPT-ANDROID / junseo-kim",
+            dataType = Companion.type2
         )
-
     )
 }
