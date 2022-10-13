f3a47380bad5a260bc9093287aca2abbc5bc6091
giovannijunseokim <kingjunseo@gmail.com>
2022-10-13 15:40:02 +0900

    [feat] Scroll To Top 기능 추가

--- app/src/main/java/org/sopt/sample/MainActivity.kt
+++ app/src/main/java/org/sopt/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package org.sopt.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
 import org.sopt.sample.fragments.GalleryFragment
 import org.sopt.sample.fragments.SearchFragment
 import org.sopt.sample.databinding.ActivityMainBinding
@@ -15,18 +16,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firstFragment()
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            changeFragment(item)
+        }
+        binding.bnvMain.setOnItemReselectedListener { item ->
+            scrollRecyclerView(item)
+        }
+    }
+
+    private fun firstFragment(){
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
         if(currentFragment == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragmentContainerView, HomeFragment())
                 .commit()
         }
-
-        binding.bnvMain.setOnItemSelectedListener { item ->
-            changeFragment(item)
-        }
     }
-
     private fun changeFragment(item : MenuItem) : Boolean{
         when(item.itemId) {
             R.id.item_home -> {
@@ -48,6 +54,14 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             else -> return false
+        }
+    }
+    private fun scrollRecyclerView(item : MenuItem){
+        when(item.itemId) {
+            R.id.item_home -> {
+                val recyclerView = findViewById<RecyclerView>(R.id.rv_github_users)
+                recyclerView.smoothScrollToPosition(0)
+            }
         }
     }
 }
